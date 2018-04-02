6482d900c1aacf0398f307b484a749fa29c8e9a9
sevenflash <sevenflash@yandex.ru>
2018-04-02 20:52:51 +0300

    questions

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const Bot = require('node-telegram-bot-api');
 
 const domains = require("./domains.json");
+const questions = require("./questions.json");
+
+const command = '/show';
+
+const devToken = '504157066:AAGPuih7wRfL6gkTUL8M9gA-9rYRDA-6Hf0';
+const prodToken = '592274947:AAEm2fSBDixNDzUVUUu11agTTpy1qJyr2Dg';
+const token = process.env.dev ? devToken : prodToken;
+
+const bot = new Bot(token, { polling: true });
 
 function containsDomain(string) {
 	let domainFound = false;
@@ -12,20 +21,34 @@ function containsDomain(string) {
 	return domainFound;
 }
 
-const token = '592274947:AAEm2fSBDixNDzUVUUu11agTTpy1qJyr2Dg';
+function reply(message) {
+	if(message.text.startsWith(command)) {
+		const topic = message.text.substr(command.length + 1);
 
-const bot = new Bot(token, { polling: true });
+		if(questions[topic]) {
+			bot.sendMessage(message.chat.id, questions[topic]);
+		}
+	}
+}
 
-bot.on('message', (message) => {
+function remove(message) {
 	if(containsDomain(message.text)) {
 		bot.getChatMember(message.chat.id, message.from.id).then((data) => {
 			if (data.status !== "creator" && data.status !== "administrator") {
 				let deletePromise = bot.deleteMessage(message.chat.id, message.message_id);
 				let kickPromise = bot.kickChatMember(message.chat.id, message.from.id);
 				Promise.all([deletePromise, kickPromise]).then(() => {
-					console.log(`[${new Date()}]`);
+					console.log(`[remove]${new Date()}`);
 				});
 			}
 		});
+	}
+}
+
+bot.on('message', (message) => {
+	if(message.chat.type === 'private') {
+		reply(message);
+	} else {
+		remove(message);
 	}
 });
